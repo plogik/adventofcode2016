44bc6c95a6bd084bab3ba425cd5ff1bed3ac063a
Stefan Emanuelsson <semanuelsson@gmail.com>
2016-12-14 21:22:07 +0100

    Removed day 1 part 2 stuff from day 1 part 1

--- Day1_1/Day1_1/Program.cs
+++ Day1_1/Day1_1/Program.cs
@@ -9,39 +9,17 @@ namespace Day1_1
 {
     class Program
     {
-		static void Part1()
-		{
-			string input = File.ReadAllText("input.txt");
-			var steps = StepsParser.Parse(input);
-			Navigator navigator = new Navigator();
-			foreach (var step in steps)
-			{
-				navigator.Move(step.Direction, step.Steps);
-			}
-			Console.WriteLine("# steps away:{0}", navigator.CurrentPosition.DistanceFromOrigo());
-			Console.ReadLine();
-		}
-
-		static void Part2()
-		{
-			string input = File.ReadAllText("input.txt");
-			var steps = StepsParser.Parse(input);
-			Navigator navigator = new Navigator();
-			var history = new List<Point>() { navigator.CurrentPosition };
-			foreach (var step in steps)
-			{
-				navigator.Move(step.Direction, step.Steps);
-				if (history.Contains(navigator.CurrentPosition))
-					break;
-				history.Add(navigator.CurrentPosition);
-			}
-			Console.WriteLine("# steps away:{0}", navigator.CurrentPosition.DistanceFromOrigo());
-			Console.ReadLine();
-		}
 		static void Main(string[] args)
         {
-			//Part1();
-			Part2();
+			string input = File.ReadAllText("input.txt");
+			var steps = StepsParser.Parse(input);
+			Navigator navigator = new Navigator();
+			foreach (var step in steps)
+			{
+				navigator.Move(step.Direction, step.Steps);
+			}
+			Console.WriteLine("# steps away:{0}", navigator.CurrentPosition.DistanceFromOrigo());
+			Console.ReadLine();
 		}
 	}
 }
