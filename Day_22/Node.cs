659c76d7e655f80b24cb1f7977145ecbc66e73af
Stefan Emanuelsson <semanuelsson@gmail.com>
2017-01-06 10:37:02 +0100

    Day 22 pt 1

---
+++ Day_22/Node.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_22
+{
+	public struct Node
+	{
+		public string Filesystem;
+		public int Size;
+		public int Used;
+		public int Avail;
+		public int UsedPercent;
+
+		public static Node From(string input)
+		{
+			var parts = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+			return new Node()
+			{
+				Filesystem = parts[0],
+				Size = int.Parse(parts[1].Substring(0, parts[1].Length - 1)),
+				Used = int.Parse(parts[2].Substring(0, parts[2].Length - 1)),
+				Avail = int.Parse(parts[3].Substring(0, parts[3].Length - 1)),
+				UsedPercent = int.Parse(parts[4].Substring(0, parts[4].Length - 1))
+			};
+		}
+	}
+}
---
+++ Day_22/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_22
+{
+	class Program
+	{
+		static int NumPairs(List<Node> nodes)
+		{
+			var numPairs = 0;
+			for(int a = 0; a < nodes.Count - 1; a++)
+			{
+				for(int b = a + 1; b < nodes.Count; b++)
+				{
+					if (nodes[a].Used > 0 && nodes[a].Used <= nodes[b].Avail)
+						numPairs++;
+					if (nodes[b].Used > 0 && nodes[b].Used <= nodes[a].Avail)
+						numPairs++;
+
+				}
+			}
+			return numPairs;
+		}
+
+		static void Main(string[] args)
+		{
+			var nodes = new List<Node>();
+			foreach(var line in File.ReadLines("input.txt"))
+			{
+				nodes.Add(Node.From(line));
+			}
+			Console.WriteLine("# pairs:" + NumPairs(nodes));
+			Console.ReadLine();
+		}
+	}
+}
