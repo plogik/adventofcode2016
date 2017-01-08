178308910b3b1aedaf3ceda8717b2c32ffb7f960
Stefan Emanuelsson <semanuelsson@gmail.com>
2017-01-08 20:50:57 +0100

    Day 25

---
+++ Day_25/Computer.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day_25
+{
+	// Modified computer from day 23 (and 12)
+	public class Computer
+	{
+		private Dictionary<string, long> registers = new Dictionary<string, long>()
+		{
+			{ "a", 0 }, 
+			{ "b", 0 },
+			{ "c", 0 },
+			{ "d", 0 }
+		};
+		private string[] strOps;
+
+		public Computer(string[] strOps)
+		{
+			this.strOps = strOps;
+		}
+
+		public int[] Run(int sequenceLen)
+		{
+			var seqIdx = 0;
+			var buf = new int[sequenceLen];
+			int sp = 0;
+			while(sp < strOps.Length)
+			{
+				var parts = strOps[sp].Split(' ');
+
+				switch (parts[0])
+				{
+					case "tgl":
+						var idx = sp + registers[parts[1]];
+						if(idx < strOps.Length) // inside program bounds?
+						{
+							var strOp = strOps[idx];
+							var toToggle = strOp.Split(' ')[0];
+							var toggledOp = 
+								toToggle == "cpy" ? "jnz" :
+								toToggle == "jnz" ? "cpy" :
+								toToggle == "inc" ? "dec" : "inc";
+							strOps[idx] = toggledOp + strOps[idx].Substring(3);
+						}
+						break;
+					case "cpy":
+						// Skip if the new tgl instruction has messed this into a non-valid instruction 
+						// (does not happen on my input though)
+						if (!(char.IsDigit(parts[1][0]) && char.IsDigit(parts[2][0])))
+						{
+							registers[parts[2]] = (char.IsDigit(parts[1][0]) || parts[1][0] == '-') ?
+								long.Parse(parts[1]) : registers[parts[1]];
+						}
+						break;
+					case "inc":
+						registers[parts[1]]++;
+						break;
+					case "dec":
+						registers[parts[1]]--;
+						break;
+					case "jnz":
+						// Jump only if literal or content of reg is not 0
+						if((char.IsDigit(parts[1][0]) ?
+							long.Parse(parts[1]) : registers[parts[1]]) != 0)
+						{
+							// Jump by literal (isDigit or starts with '-') or by content of reg
+							sp += (int)((char.IsDigit(parts[2][0]) || parts[2][0] == '-') ?
+								long.Parse(parts[2]) : registers[parts[2]]);
+							sp--;
+						}
+						break;
+					case "out":
+						buf[seqIdx] = (int)registers[parts[1]];
+						seqIdx++;
+						if (seqIdx == sequenceLen)
+							return buf;
+						break;
+				}
+				sp++;
+			}
+			return null;
+		}
+
+		public long this [string reg]
+		{
+			set
+			{
+				registers[reg] = value;
+			}
+			get
+			{
+				return registers[reg];
+			}
+		}
+	}
+}
---
+++ Day_25/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_25
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var sw = new Stopwatch();
+			sw.Start();
+			var computer = new Computer(File.ReadAllLines("input.txt"));
+			int seed = 0;
+			var found = false;
+			while(!found)
+			{
+				seed++;
+				computer["a"] = seed;
+				computer["b"] = 0;
+				computer["c"] = 0;
+				computer["d"] = 0;
+				var result = computer.Run(80);
+				if (IsCorrectSequence(result))
+					found = true;
+			}
+			sw.Stop();
+			Console.WriteLine("Correct seed:" + seed);
+			Console.WriteLine("Elapsed:" + sw.Elapsed);
+			Console.ReadLine();
+		}
+
+		static bool IsCorrectSequence(int[] input)
+		{
+			int prev = 1;
+			foreach(var i in input)
+			{
+				if (i == prev)
+					return false;
+				prev = i;
+			}
+			return true;
+		}
+	}
+}
