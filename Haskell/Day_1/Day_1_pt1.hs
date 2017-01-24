33f7534eb192d016eaa54ebfa5deb17300995ea5
Stefan Emanuelsson <semanuelsson@gmail.com>
2017-01-24 21:02:18 +0100

    Day 1 pt2

--- Haskell/Day_1/Day_1_pt1.hs
+++ Haskell/Day_1/Day_1_pt1.hs
@@ -33,6 +33,9 @@ moveAll d p (x:xs) = do
 distance :: Point -> Int
 distance = (\(x, y) -> (abs x) + (abs y))
 
-input = "R4, R4, L1, R3, L5, R2, R5, R1, L4, R3, L5, R2, L3, L4, L3, R1, R5, R1, L3, L1, R3, L1, R2, R2, L2, R5, L3, L4, R4, R4, R2, L4, L1, R5, L1, L4, R4, L1, R1, L2, R5, L2, L3, R2, R1, L194, R2, L4, R49, R1, R3, L5, L4, L1, R4, R2, R1, L5, R3, L5, L4, R4, R4, L2, L3, R78, L5, R4, R191, R4, R3, R1, L2, R1, R3, L1, R3, R4, R2, L2, R1, R4, L5, R2, L2, L4, L2, R1, R2, L3, R5, R2, L3, L3, R3, L1, L1, R5, L4, L4, L2, R5, R1, R4, L3, L5, L4, R5, L4, R5, R4, L3, L2, L5, R4, R3, L3, R1, L5, R5, R1, L3, R2, L5, R5, L3, R1, R4, L5, R4, R2, R3, L4, L5, R3, R4, L5, L5, R4, L4, L4, R1, R5, R3, L1, L4, L3, L4, R1, L5, L1, R2, R2, R4, R4, L5, R4, R1, L1, L1, L3, L5, L2, R4, L3, L5, L4, L1, R3"
-pt1 = distance $ moveAll N (0,0) $ parseInput input
+pt1 :: String -> Int
+pt1 s = distance $ moveAll N (0,0) $ parseInput s
     
+main = do
+    input <- readFile "input.txt"
+    putStrLn $ "Distance: " ++ show (pt1 input)
---
+++ Haskell/Day_1/Day_1_pt2.hs
@@ -0,0 +1,49 @@
+import Data.Maybe
+
+data Direction = N | E | S | W deriving (Show, Eq, Ord)
+data Turn = L | R deriving (Read, Show, Eq, Ord)
+
+type Point = (Int, Int)
+type Steps = Int
+
+newDirection :: Direction -> Turn -> Direction
+newDirection N L = W
+newDirection N R = E
+newDirection E L = N
+newDirection E R = S
+newDirection S L = E
+newDirection S R = W
+newDirection W L = S
+newDirection W R = N
+
+move :: Point -> Direction -> Steps -> [Point]
+move (x, y) N s = [(x, y + n) | n <- [1..s]]
+move (x, y) W s = [(x - n, y) | n <- [1..s]]
+move (x, y) S s = [(x, y - n) | n <- [1..s]]
+move (x, y) E s = [(x + n, y) | n <- [1..s]]
+
+parseInput :: String -> [(Turn, Steps)]
+parseInput s = map (\(x:xs) -> (read [x] :: Turn, read xs :: Steps)) $ 
+            map (takeWhile (/=',')) $ words s
+
+moveAll :: Direction -> Point -> [(Turn, Steps)] -> [Point] -> [Point]
+moveAll _ _ [] ps = ps -- no more steps
+moveAll d p (x:xs) ps = do
+    let nd = newDirection d (fst x)
+        nps = move p nd (snd x)
+    moveAll nd (last nps) xs (ps ++ nps)
+
+firstDup :: [Point] -> Maybe Point
+firstDup [] = Nothing
+firstDup (x:xs)
+    | x `elem` xs = Just x
+    | otherwise = firstDup xs
+
+distance :: Point -> Int
+distance = (\(x, y) -> (abs x) + (abs y))
+
+main = do
+    input <- readFile "input.txt"
+    let maybeDup = firstDup $  moveAll N (0,0) (parseInput input) [(0, 0)]
+    putStrLn $ "First dup: " ++ show (distance (fromJust maybeDup))
+
