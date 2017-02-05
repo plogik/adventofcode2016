adc380b46c49ef772adacbedb6b8d6643cf91eba
Stefan Emanuelsson <semanuelsson@gmail.com>
2017-02-05 09:58:32 +0100

    Not using testTriangles anymore

--- Haskell/Day_3/Day_3_pt1.hs
+++ Haskell/Day_3/Day_3_pt1.hs
@@ -1,25 +1,35 @@
-isRightTriangle :: (Ord a, Num a) => (a, a, a) -> Bool
+import System.CPUTime
+import Text.Printf
+
+type Triangle =  (Int, Int, Int)
+
+isRightTriangle :: Triangle -> Bool
 isRightTriangle (x, y, z) =
-    (x < (y + z)) &&
-    (y < (x + z)) &&
-    (z < (x + y))
+    x < (y + z) &&
+    y < (x + z) &&
+    z < (x + y)
 
-toTuple :: [Int] -> (Int, Int, Int)
-toTuple [x, y, z] = (x, y, z)
+toTriangle :: [Int] -> Triangle
+toTriangle [x, y, z] = (x, y, z)
 
-parseTriangle :: String -> (Int, Int, Int)
-parseTriangle n = toTuple $ map (read::String->Int) (words n)
+parseTriangle :: String -> Triangle
+parseTriangle n = toTriangle $ map (read::String->Int) (words n)
 
+-- This function is not used
 testTriangles :: [String] -> Int
 testTriangles s = go s 0 
     where go [] n = n
           go (x:xs) n = 
             if isRightTriangle $ parseTriangle x 
-                then go xs (n + 1) 
+                then go xs (n + 1)
                 else go xs n
 
 
 main = do
+    start <- getCPUTime
     input <- readFile "input.txt"
-    let n = testTriangles $ lines input
-    putStrLn $ "Correct # triangles:" ++ show n
+    --let n = testTriangles $ lines input
+    let n = length . filter isRightTriangle . map parseTriangle $ lines input
+    end <- getCPUTime
+    let diff = (fromIntegral (end - start)) / (10^12)
+    printf "# correct triangles: %d, time: %0.5fs\n" (n::Int) (diff :: Double)
