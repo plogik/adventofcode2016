02f96132d0945e5c44ad23239e78883e7b0f8360
Stefan Emanuelsson <semanuelsson@gmail.com>
2017-02-08 19:54:33 +0100

    A little more readable

--- Haskell/Day_3/Day_3_pt2.hs
+++ Haskell/Day_3/Day_3_pt2.hs
@@ -17,10 +17,14 @@ rowsToCols :: [Triangle] -> [Triangle]
 rowsToCols [] = []
 rowsToCols [(x1, y1, z1), (x2, y2, z2), (x3, y3, z3)] = [(x1, x2, x3), (y1, y2, y3), (z1, z2, z3)]
 
+rightTriangleCount :: [Triangle] -> Int
+rightTriangleCount [] = 0
+rightTriangleCount xs =  length $ filter isRightTriangle xs
+
 testTriangles' :: [Triangle] -> Int
 testTriangles' ts = go ts 0
     where go [] n = n
-          go (x:y:z:xs) n = go xs (n + (length . filter isRightTriangle . rowsToCols $ [x,y,z]))
+          go (x:y:z:xs) n = go xs (n + (rightTriangleCount . rowsToCols $ [x,y,z]))
 
 main = do
     input <- readFile "input.txt"
